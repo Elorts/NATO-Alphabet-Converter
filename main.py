0de45620f34c5805a9c23392c0b49fc7f5262f68
Deividas Strole <deiwis@yahoo.com>
2023-03-17 12:38:09 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,11 @@
 import pandas
 
-#TODO 1. Create a dictionary in this format:
-#{"A": "Alfa", "B": "Bravo"}
-
-#easy methood
-#data_frame = pandas.read_csv("nato_phonetic_alphabet.csv", header=None, index_col=0, squeeze=True)
-
+#Read file of codes and create a dictionary
 df = pandas.read_csv("nato_phonetic_alphabet.csv")
-dic = df.to_dict()
-print(dic)
+dic = {row.letter: row.code for (index, row) in df.iterrows()}
 
-#TODO 2. Create a list of the phonetic code words from a word that the user inputs.
-
-# user_input = input("Enter any word: ").upper()
-# input_list = list(user_input)
-# list_of_words = [dic[letter] for letter in input_list]
-# print(list_of_words)
-
+#Convert user input to list and translate each letter into code
+user_input = input("Enter any word: ").upper()
+input_list = list(user_input)
+list_of_words = [dic[letter] for letter in input_list]
+print(list_of_words)
